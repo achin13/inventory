d84456aa6307dac8abca1b239028fb1e68d7392d
achin13 <88187889+achin13@users.noreply.github.com>
2021-08-01 17:42:54 +0530

    Add files via upload

---
+++ codedeploy/afterInstall.sh
@@ -0,0 +1,11 @@
+#echo "***************************************************"
+#echo "AFTER INSTALL BEGINS"
+#echo "***************************************************"
+#
+#
+## make next file executable
+#sudo chmod 777 /var/www/html/codedeploy/applicationStart.sh
+#
+#echo "***************************************************"
+#echo "AFTER INSTALL ENDS"
+#echo "***************************************************"
---
+++ codedeploy/beforeInstall.sh
@@ -0,0 +1,22 @@
+#echo "***************************************************"
+#echo "BEFORE INSTALL BEGINS"
+#echo "***************************************************"
+#
+#
+#
+#
+#
+#
+#
+#
+### kill tomcat process (redundant command, just to be safe)
+##sudo pkill -9 -f tomcat
+#
+#
+### make next file executable
+###sudo chmod 777 /home/ubuntu/codedeploy/install.sh
+##
+##
+#echo "***************************************************"
+#echo "BEFORE INSTALL ENDS"
+#echo "***************************************************"
