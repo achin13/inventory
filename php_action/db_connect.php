23a4f5228c85a9205320cf25b1d87b94bc493e14
achin13 <88187889+achin13@users.noreply.github.com>
2021-08-01 13:13:58 +0530

    Update db_connect.php

--- php_action/db_connect.php
+++ php_action/db_connect.php
@@ -4,7 +4,7 @@ $localhost = "store.cseraeacxjps.ap-south-1.rds.amazonaws.com";
 $username = "admin";
 $password = "store123";
 $dbname = "store";
-$store_url = "Inventory-1696891882.ap-south-1.elb.amazonaws.com";
+$store_url = "/";
 // db connection
 $connect = new mysqli($localhost, $username, $password, $dbname);
 // check connection
