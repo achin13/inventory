2747ba2466b0cae27136d40b0e25ac91a29f8e0b
achin13 <88187889+achin13@users.noreply.github.com>
2021-08-01 13:06:50 +0530

    Update db_connect.php

--- php_action/db_connect.php
+++ php_action/db_connect.php
@@ -1,10 +1,10 @@
 <?php 	
 
-$localhost = "localhost";
-$username = "root";
-$password = "root";
+$localhost = "store.cseraeacxjps.ap-south-1.rds.amazonaws.com";
+$username = "admin";
+$password = "store123";
 $dbname = "store";
-$store_url = "http://localhost/php-inventory-management-system/";
+$store_url = "Inventory-1696891882.ap-south-1.elb.amazonaws.com";
 // db connection
 $connect = new mysqli($localhost, $username, $password, $dbname);
 // check connection
